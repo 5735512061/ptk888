6d7a4132bb7f1330942010cc51c62aa1face51f8
5735512061 <hathaichanok42@hotmail.com>
2021-04-29 17:20:11 +0700

    update 29/04/2564

--- resources/views/authSeller/login.blade.php
+++ resources/views/authSeller/login.blade.php
@@ -1,22 +1,32 @@
 @extends("/backend/layouts/template/template-admin-login")
-
+<style>
+    .btn:hover{
+        color: #fff !important;
+        background: #ff882e !important;
+    }
+    .btn{
+        color: #fff !important;
+        background: #7b7b7b !important;
+        border: 1px solid #fff !important;
+    }
+</style>
 @section('content')
 <!-- Login Start -->
 <div class="login">
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-lg-5">
-                <div style="font-size: 6rem; text-align:center; color:#bdbdbda6;">
-                    <i class="fa fa-lock"></i> <i class="fa fa-arrow-right"></i> <i class="fa fa-unlock-alt"></i>
+                <div style="font-size: 6rem; text-align:center; color:#7b7b7b;">
+                    <i class="fa fa-lock"></i> <i style="color:#ff882e;" class="fa fa-arrow-right"></i> <i class="fa fa-unlock-alt"></i>
                 </div>
                 
-                <div class="login-form">
-                    <h3>เข้าสู่ระบบพนักงานขาย</h3><hr>
+                <div class="login-form" style="background: #7b7b7b;">
+                    <h3 style="color: #fff; font-weight: bold; text-align:center;">เข้าสู่ระบบพนักงานขาย</h3><hr>
                     <form action="{{url('/seller/login')}}" enctype="multipart/form-data" method="post">@csrf
                         @csrf
 
                         <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">{{ __('ชื่อเข้าใช้งาน') }}</label>
+                            <label class="col-md-4 col-form-label text-md-right" style="color: #fff; font-weight: bold;">{{ __('รหัสสมาชิก') }}</label>
 
                             <div class="col-md-6">
                                 @if ($errors->has('seller_id'))
@@ -27,7 +37,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('รหัสผ่าน') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right" style="color: #fff; font-weight: bold;">{{ __('รหัสผ่าน') }}</label>
 
                             <div class="col-md-6">
                                 @if ($errors->has('password'))
--- resources/views/authStore/login.blade.php
+++ resources/views/authStore/login.blade.php
@@ -1,22 +1,27 @@
 @extends("/backend/layouts/template/template-admin-login")
-
+<style>
+    .btn:hover{
+        color: #fff !important;
+        background: #7b7b7b !important;
+    }
+</style>
 @section('content')
 <!-- Login Start -->
 <div class="login">
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-lg-5">
-                <div style="font-size: 6rem; text-align:center; color:#bdbdbda6;">
-                    <i class="fa fa-lock"></i> <i class="fa fa-arrow-right"></i> <i class="fa fa-unlock-alt"></i>
+                <div style="font-size: 6rem; text-align:center; color:#ff882e;">
+                    <i class="fa fa-lock"></i> <i style="color:#7b7b7b;" class="fa fa-arrow-right"></i> <i class="fa fa-unlock-alt"></i>
                 </div>
                 
-                <div class="login-form">
-                    <h3>เข้าสู่ระบบสมาชิกร้านค้า</h3><hr>
+                <div class="login-form" style="background: #ff882e;">
+                    <h3 style="color: #fff; font-weight: bold; text-align:center;">เข้าสู่ระบบสมาชิกร้านค้า</h3><hr>
                     <form action="{{url('/store/login')}}" enctype="multipart/form-data" method="post">@csrf
                         @csrf
 
                         <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">{{ __('ชื่อเข้าใช้งาน') }}</label>
+                            <label class="col-md-4 col-form-label text-md-right" style="color: #fff; font-weight: bold;">{{ __('รหัสสมาชิก') }}</label>
 
                             <div class="col-md-6">
                                 @if ($errors->has('store_id'))
@@ -27,7 +32,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('รหัสผ่าน') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right" style="color: #fff; font-weight: bold;">{{ __('รหัสผ่าน') }}</label>
 
                             <div class="col-md-6">
                                 @if ($errors->has('password'))
@@ -39,7 +44,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn">
+                                <button type="submit" class="btn" style="font-weight: bold;">
                                     {{ __('เข้าสู่ระบบสมาชิกร้านค้า') }}
                                 </button>
                             </div>
